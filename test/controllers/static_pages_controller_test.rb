99d58d41b8bd8795b3fe017be343a47d1ea31462
Yuan Xue <yuanxue68@gmail.com>
2015-10-19 21:15:30 -0400

    added user model test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
-
+include Devise::TestHelpers
 class StaticPagesControllerTest < ActionController::TestCase
+	include Devise::TestHelpers
   test "should get home" do
     get :home
     assert_response :success
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	#include Devise::TestHelpers
+  def setup
+  	@user1 = User.new(email: "yuanxue68@gmail.com", password: "password", password_confirmation: "password")
+  	@user2 = User.new(email: "yuanxue68@gmail.com", password: "password", password_confirmation: "password1")
+  end
+
+  test "should be valid" do
+  	assert @user1.valid?
+  end
+
+  test "unmatching password should not be valid" do
+  	assert_not @user2.valid?
+  end
+
+  test "empty email should not be valid" do
+  	@user1.email="   "
+  	assert_not @user1.valid?
+  end
 end
