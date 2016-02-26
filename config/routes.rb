b6dcf87f077821accb1e538d78d4290e29a390eb
Yuan Xue <yuanxue68@gmail.com>
2016-02-25 22:41:45 -0500

    added styling to footer and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :auctions
   # The priority is based upon order of creation: first created -> highest priority.
