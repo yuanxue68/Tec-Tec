d95ed2aa87ed7329ea923c8cb8dc79b52e4997a0
Yuan Xue <yuanxue68@gmail.com>
2016-03-05 23:12:05 -0500

    added spec for filter

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,7 +5,7 @@ require 'byebug'
 RSpec.describe ApplicationHelper, type: :helper do
   it "should return active" do
     controller.params[:action] = 'show'
-    expect(helper.is_active?("show")).to eq("active")
+    expect(helper.is_active?(:action, "show")).to eq("active")
   end
 end
 
--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 require 'spec_helper'
+require 'byebug'
 
 RSpec.describe Auction, type: :model do
+  before :all do
+    user1 = FactoryGirl.build(:user, email: "test2.gmail.com")
+    user2 = FactoryGirl.build(:user, email: "test3.gmail.com")
+    @auction_one = FactoryGirl.create(:auction, name: "auction one",
+                                      owner: user1, end_time: Time.now )
+    @auction_two = FactoryGirl.create(:auction, name: "auction two",
+                                      owner: user2, end_time: Time.now+500)
+  end
+
   it "has a valid factory" do
     auction = FactoryGirl.build(:auction)
     expect(auction).to be_valid
@@ -25,5 +35,21 @@ RSpec.describe Auction, type: :model do
   it "should have a end time" do
     auction = FactoryGirl.build(:auction, end_time:nil)
     expect(auction).to_not be_valid
+  end
+  
+  it "should be ordered by end time" do
+    expect(Auction.order_by_created_at).to be == [@auction_two, @auction_one] 
+  end
+
+  it "should be ordered by created at" do
+    expect(Auction.order_by_ending).to be == [@auction_one, @auction_two]
+  end
+
+  it "should filter based on search value" do
+    expect(Auction.ordered_search(nil, "one")).to be == [@auction_one]
+  end
+
+  it "should be able to filter with empty string" do
+    expect(Auction.ordered_search("ending","")).to be == [@auction_one, @auction_two]
   end
 end
