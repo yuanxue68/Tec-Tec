50db08654f1affa86a7d66eb2eddca99cf822410
Yuan Xue <yuanxue68@gmail.com>
2016-03-20 23:03:25 -0400

    add send grid environment variable

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,12 @@ require File.expand_path('../application', __FILE__)
 Rails.application.initialize!
 
 require 'carrierwave/orm/activerecord'
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
+  :domain => 'floating-mesa-3668.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
