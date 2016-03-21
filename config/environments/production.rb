dcd7329f3b842ad4a364640a5d22623f0079d728
Yuan Xue <yuanxue68@gmail.com>
2016-03-20 23:19:39 -0400

    add action mailer url in prodution.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'floating-mesa-3668.herokuapp.com', port: ENV["port"] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+
+
 end
